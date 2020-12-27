b8f03d847d64b9d70a9c324eda054eeb835ce29e
bndeepankan <bn.deepankan@gmail.com>
2020-12-27 22:40:35 +0100

    added changes

---
+++ greeter.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"greeter.h"
+#include<string>
+
+using namespace std;
+
+void greet(std::string name){
+
+	std::cout<<name<<endl;
+}
+
+void bye(std::string name){
+	std::cout<<name<<endl;
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "greeter.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{	
+
+	greet("Hello world");
+	bye("bye world");
+	return 0;
+}
